12a8ca9f4e4b21d08afdf3b98fcdb7258e20ad5f
vitas <lcztt@users.noreply.github.com>
2025-08-14 15:19:06 +0800

    update jenkins run platform

--- Products/Scripts/Build/pipeline_ios.groovy
+++ Products/Scripts/Build/pipeline_ios.groovy
@@ -57,4 +57,4 @@ def doPublish(buildVariables) {
     sh "rm -rf *.zip || true"
 }
 
-pipelineLoad(this, "Proctor_iOS", "build", "ios", "shadow")
+pipelineLoad(this, "Proctor_iOS", "build", "ios", "shadow && mac")
